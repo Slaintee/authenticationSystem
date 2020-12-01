12ac05427154f5b53190a9413531913f6144d057
Ken Zhang <yzhang76@uvm.edu>
2020-12-01 23:43:19 +0800

    Comments

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ CS 166 / Fall 2020
 """
 
 from authentication import password_strength
+from config import display
+import csv
+from flask import Flask, render_template, request, url_for, flash, redirect
+from db import Db
 from password_crack import hash_pw, authenticate
 import random
 import sqlite3
 import string
+
+app = Flask(__name__, static_folder='instance/static')
+
+app.config.from_object('config')
 
 
 def create_db():
@@ -33,10 +41,11 @@ def create_db():
             conn.close()
 
 
-def add_user():
-    """ Example data insert into users table """
+def add_user(user):
+    """ Insert new user into users table """
     try:
         print("Do not enter quotation mark otherwise it will be removed.")
+        # Ask for new username and password, and prevent from sql injection
         new_username = str(input("Please enter new username: "))
         new_username = sql_injection(new_username)
         new_password = str(input("Please enter new password in length of between 8 and 25, \n"
@@ -47,6 +56,7 @@ def add_user():
             new_password = str(input("Invalid input. Enter again: "))
         new_password = sql_injection(new_password)
         new_password = hash_pw(new_password)
+        # Set new user as least level
         new_access = "1"
         data_to_insert = [(new_username, new_password, new_access)]
     except ValueError:
@@ -55,6 +65,7 @@ def add_user():
     try:
         conn = sqlite3.connect('user.db')
         c = conn.cursor()
+        # Insert new user to database
         c.executemany("INSERT INTO users VALUES (?, ?, ?)", data_to_insert)
         conn.commit()
     except sqlite3.IntegrityError:
@@ -69,6 +80,11 @@ def add_user():
 
 
 def generate_password():
+    """
+    Generate a new random password satisfies the requirement
+
+    :return: str
+    """
     src = string.ascii_letters + string.digits + "!@#$%^&*"
     # Make random length of password
     num = random.randint(4, 21)
@@ -82,15 +98,20 @@ def generate_password():
     list_passwd_all.extend(random.sample(string.ascii_uppercase, 1))
     # Make it includes special character
     list_passwd_all.extend(random.sample("!@#$%^&*", 1))
-    # Shuffle the list and make it to string
+    # Shuffle the list and change it to string
     random.shuffle(list_passwd_all)
     str_passwd = ''.join(list_passwd_all)
     return str_passwd
 
 
-def login(username, password):
-    """Given user name and password,
-    return message or main menu"""
+def login(username, password, user):
+    """
+    Given user name and password,
+    return message or main menu
+
+    :param username: str
+    :param password: str
+    """
     try:
         conn = sqlite3.connect("user.db")
         c = conn.cursor()
@@ -183,20 +204,23 @@ def sql_injection(value):
     return value
 
 
-def enter():
+def enter(user):
+    # User has 3 times to enter the existing account
     max_time = 3
     logged_in = False
     while not logged_in:
+        # Enter the username and password, and prevent from sql injection
         print("------------ Login ------------")
         username = input("Enter your username: ").strip()
         username = sql_injection(username)
         password = input("Enter your password: ").strip()
         password = sql_injection(password)
-        logged_in = login(username, password)
+        logged_in = login(username, password, user)
         if logged_in:
             print('------- welcome, {} -------'.format(username))
             break
         else:
+            # If account invalid more than 3 times, system out
             max_time -= 1
             print("Incorrect user name or password.", max_time, "time(s) left.")
             if max_time == 0:
@@ -205,11 +229,11 @@ def enter():
     return username
 
 
-if __name__ == "__main__":
+# @app.route("/", methods=['GET', 'POST'])
+def home():
     """Given user area choice, check user level
     and return whether validated or whether succeed"""
-    # Print head
-    print("Welcome, enter the user name and password to log in thr system.")
+
     # Authenticate user
     create_db()
     user = {}
@@ -220,18 +244,18 @@ if __name__ == "__main__":
             login_choice = int(input("1. Log in\n2. Register\n->"))
             if login_choice == 1:
                 # Login
-                username = enter()
+                username = enter(user)
                 break
             elif login_choice == 2:
-                add_user()
+                add_user(user)
                 # Login
-                username = enter()
+                username = enter(user)
                 break
             else:
-                # avoid anything other than 1 and 2 entered
+                # Stop entering anything other than 1 and 2
                 invalid()
         except ValueError:
-            # avoid anything other than number entered
+            # Stop entering anything other than number
             invalid()
 
     # Ask login function for the access level
@@ -299,3 +323,6 @@ if __name__ == "__main__":
     except ValueError:
         invalid()
         exit()
+
+
+home()
